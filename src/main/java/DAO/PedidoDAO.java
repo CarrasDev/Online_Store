acf204a9334774de3ab9bdcd6e836a3c57367f77
CarrasDev <carrascoluque@gmail.com>
2025-04-29 13:32:22 +0200

    Implementar PedidoDAO getAll()

--- src/main/java/DAO/PedidoDAO.java
+++ src/main/java/DAO/PedidoDAO.java
@@ -12,12 +12,12 @@ public class PedidoDAO implements IDao<Pedido> {
 
     @Override
     public Optional<Pedido> getById(String id) {
-        // TODO Pendiente implementar
+
         try (Session session = HibernateUtil.getSessionFactory().openSession()) {
             Pedido pedido = session.get(Pedido.class, Integer.parseInt(id));  //Integer.parseInt(id) --> de string a Int
             return Optional.ofNullable(pedido);
         } catch (Exception e) {
-            System.err.println("Error al acceder a la BBDD con id: " + id + " " + e.getMessage());
+            System.err.println("Error al acceder al Pedido con id: " + id + " " + e.getMessage());
         }
         return Optional.empty();
     }
@@ -26,6 +26,11 @@ public class PedidoDAO implements IDao<Pedido> {
     @Override
     public List<Pedido> getAll() {
         // TODO Pendiente implementar
+        try (Session session = HibernateUtil.getSessionFactory().openSession()) {
+            return session.createQuery("FROM Pedido", Pedido.class).getResultList(); // JPA
+        } catch (Exception e) {
+            System.err.println("Error al acceder a todos los Pedidos de la BBDD " + e.getMessage());
+        }
         return List.of();
     }
 
