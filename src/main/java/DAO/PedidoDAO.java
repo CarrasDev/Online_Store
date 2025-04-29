df1efd42c3da20a819050a9d3e2b5697229d30e4
CarrasDev <carrascoluque@gmail.com>
2025-04-29 13:26:29 +0200

    Implementar PedidoDAO getById()

--- src/main/java/DAO/PedidoDAO.java
+++ src/main/java/DAO/PedidoDAO.java
@@ -1,6 +1,8 @@
 package DAO;
 
 import modelo.Pedido;
+import org.hibernate.Session;
+import util.HibernateUtil;
 
 import java.util.List;
 import java.util.Optional;
@@ -11,6 +13,12 @@ public class PedidoDAO implements IDao<Pedido> {
     @Override
     public Optional<Pedido> getById(String id) {
         // TODO Pendiente implementar
+        try (Session session = HibernateUtil.getSessionFactory().openSession()) {
+            Pedido pedido = session.get(Pedido.class, Integer.parseInt(id));  //Integer.parseInt(id) --> de string a Int
+            return Optional.ofNullable(pedido);
+        } catch (Exception e) {
+            System.err.println("Error al acceder a la BBDD con id: " + id + " " + e.getMessage());
+        }
         return Optional.empty();
     }
 
