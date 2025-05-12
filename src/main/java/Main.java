026bfa88ad987fdbdf2f5a49c056b22c03a3cb63
CarrasDev <carrascoluque@gmail.com>
2025-05-12 16:05:01 +0200

    Revisión de flujo y esbozo clase ArticuloController

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -8,10 +8,17 @@ import controlador.Controlador;
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
+
+        // TODO Revisar secuencia de arranque de MVC
         Modelo modelo = new Modelo();
         Vista vista = new Vista();
         Controlador controlador = new Controlador(modelo, vista);
+
+
+        // Inicia la conexión a la BBDD
         HibernateUtil.getSessionFactory().openSession();
+
+        // TODO --> Es clave para el arranque del programa
         vista.startVista(controlador);
     }
 }
---
+++ src/main/java/controlador/ArticuloController.java
@@ -0,0 +1,50 @@
+package controlador;
+
+import modelo.Articulo;
+
+import java.util.List;
+
+public class ArticuloController {
+
+    // TODO Desarrollo ArticuloController
+
+    // TODO variables --> Modelo y Vista correspondiente
+
+    // TODO Constructor
+
+    public void addArticulo(String codigoArticulo, String descripcion, Float precioVenta, Float gastosEnvio, Integer tiempoPreparacion) {
+        Articulo articulo = new Articulo(codigoArticulo, descripcion, precioVenta, gastosEnvio, tiempoPreparacion);
+        modeloTienda.addArticulo(articulo);
+    }
+    //LEE MODELO:Busca un articulo en el modelo y devuelve TRUE si lo encuentra
+    public boolean existeArticulo(String codigoArticulo) {
+        return (modeloTienda.existeArticulo(codigoArticulo));
+    }
+    
+    //LEE MODELO:Obtiene y retorna un listado de todos los articulos del modelo
+    private <E> List<E> getListaArticulos() {
+        return ((List<E>) modeloTienda.getArticulos());
+    }
+
+    //ACTUALIZA VISTA: Elabora listado de artículos y actualiza vista para mostrarlos
+    public <E> void mostrarArticulos() {
+        List<E> listaArticulos = this.getListaArticulos();
+        StringBuilder sb = new StringBuilder();
+        for (E articulo : listaArticulos) {
+            sb.append(articulo.toString()).append("\n");
+        }
+        vistaTienda.updateView(sb.toString());
+    }
+
+    //Comprueba si un codigo de artículo existe y procede con
+    // el proceso de creacion o actualiza vista para mostrar duplicado
+    public void nuevoCodigoArticulo(String codigoArticulo){
+        if (!existeArticulo(codigoArticulo)){
+            vistaTienda.pedirDatosArticulo(codigoArticulo);
+        } else{
+            vistaTienda.updateView("Artículo duplicado\n");
+        }
+    }
+
+
+}
