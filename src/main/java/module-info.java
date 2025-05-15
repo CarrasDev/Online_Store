d93cd56d956193a9f91f5a2b3652b6b84b579ecc
CarrasDev <carrascoluque@gmail.com>
2025-05-15 22:27:51 +0200

    Resolver conflictos arranque BBDD -> ok

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -7,9 +7,13 @@ module com.sqlsquad.onlinestore {
     requires java.naming;
 
     opens com.sqlsquad.onlinestore to javafx.fxml;
+    opens com.sqlsquad.onlinestore.controlador to javafx.fxml;
     opens com.sqlsquad.onlinestore.modelo to org.hibernate.orm.core;
+    opens com.sqlsquad.onlinestore.modelo.entity.cliente to org.hibernate.orm.core;
+    opens com.sqlsquad.onlinestore.modelo.entity to org.hibernate.orm.core;
+
     exports com.sqlsquad.onlinestore;
     exports com.sqlsquad.onlinestore.modelo;
     exports com.sqlsquad.onlinestore.controlador;
-    opens com.sqlsquad.onlinestore.controlador to javafx.fxml;
+
 }
