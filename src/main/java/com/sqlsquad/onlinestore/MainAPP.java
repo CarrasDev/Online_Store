b14c45e4db180cb6c771d6cc6546ff370b6eef9b
CarrasDev <carrascoluque@gmail.com>
2025-05-15 12:46:23 +0200

    TODOs y refactorizar hibernate.cfg.xml

--- src/main/java/com/sqlsquad/onlinestore/MainAPP.java
+++ src/main/java/com/sqlsquad/onlinestore/MainAPP.java
@@ -10,11 +10,17 @@ import java.io.IOException;
 public class MainAPP extends Application {
     @Override
     public void start(Stage stage) throws IOException {
+
+        // TODO Integrar conexión BBBDD
+
         FXMLLoader fxmlLoader = new FXMLLoader(MainAPP.class.getResource("main-view.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), 900, 500);
         stage.setTitle("SQLSquad Online Store - JavaFX");
         stage.setScene(scene);
         stage.show();
+
+        // TODO Integrar cierre BBDD
+
     }
 
     public static void main(String[] args) {
