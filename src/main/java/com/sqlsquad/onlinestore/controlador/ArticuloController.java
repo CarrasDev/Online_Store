fda2bd06f7d69218a3d15d8b596859cea5e82945
CarrasDev <carrascoluque@gmail.com>
2025-05-18 17:02:24 +0200

    Control de TODOs en ArticuloController

--- src/main/java/com/sqlsquad/onlinestore/controlador/ArticuloController.java
+++ src/main/java/com/sqlsquad/onlinestore/controlador/ArticuloController.java
@@ -21,20 +21,11 @@ public class ArticuloController {
         articuloModel.addArticulo(articulo);
     }
 
-    // TODO verificar borrado
-    /*
-    public void addArticulo(String codigoArticulo, String descripcion, Float precioVenta, Float gastosEnvio, Integer tiempoPreparacion) {
-        Articulo articulo = new Articulo(codigoArticulo, descripcion, precioVenta, gastosEnvio, tiempoPreparacion);
-        articuloModel.addArticulo(articulo);
-    }
-    */
-
     //LEE MODELO:Busca un articulo en el modelo y devuelve TRUE si lo encuentra
     public boolean existeArticulo(String codigoArticulo) {
         return (articuloModel.existeArticulo(codigoArticulo));
     }
 
-    // TODO verificar
     //LEE MODELO:Obtiene y retorna un listado de todos los articulos del modelo
     public List<Articulo> getListaArticulos() {
         return articuloModel.getArticulos();
